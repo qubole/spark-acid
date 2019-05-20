37f1dfb1c58fb118a23029684f05f5fcea61cbab
Prakhar Jain <prakharj@qubole.com>
2019-05-21 02:54:49 +0530

    Integrate Hive3Rdd - Part 1/2

--- acid-datasource/src/main/scala/com/qubole/spark/HiveAcidDataSource.scala
+++ acid-datasource/src/main/scala/com/qubole/spark/HiveAcidDataSource.scala
@@ -19,42 +19,40 @@ package com.qubole.spark
 
 import java.util.Locale
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import com.qubole.shaded.hive.conf.HiveConf
 import com.qubole.shaded.hive.metastore.HiveMetaStoreClient
-
-import scala.util.{Failure, Success, Try}
+import com.qubole.shaded.hive.metastore.api.FieldSchema
+import com.qubole.shaded.hive.metastore.utils.MetaStoreUtils.{getColumnNamesFromFieldSchema, getColumnTypesFromFieldSchema}
+import com.qubole.shaded.hive.ql.exec.Utilities
+import com.qubole.shaded.hive.ql.metadata.{Hive, HiveUtils, Table}
+import com.qubole.shaded.hive.ql.plan.TableDesc
+import com.qubole.spark.rdd.HiveRDD
+import com.qubole.spark.util.SerializableConfiguration
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.Writable
+import org.apache.hadoop.mapred.{FileInputFormat, InputFormat, JobConf}
+import org.apache.spark.{SparkContext, SparkException}
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.internal.Logging
 import org.apache.spark.rdd.RDD
-import org.apache.spark.scheduler.{SparkListener, SparkListenerApplicationStart, SparkListenerJobEnd}
 import org.apache.spark.sql._
-import org.apache.spark.sql.catalyst.TableIdentifier
-import org.apache.spark.sql.catalyst.expressions.{Expression, Literal}
-import org.apache.spark.sql.catalyst.plans.logical.SubqueryAlias
-import org.apache.spark.sql.catalyst.util.CaseInsensitiveMap
-import org.apache.spark.sql.execution.datasources.text.TextFileFormat
-import org.apache.spark.sql.execution.datasources.{CatalogFileIndex, HadoopFsRelation, TextBasedFileFormat}
-import org.apache.spark.sql.execution.streaming.{Sink, Source}
-import org.apache.spark.sql.sources._
-import org.apache.spark.sql.streaming.OutputMode
-import org.apache.spark.sql.types.{DataType, MetadataBuilder, StructField, StructType}
-import org.apache.spark.sql.execution.QueryExecution
-import org.apache.spark.sql.util.QueryExecutionListener
-import org.apache.spark.sql.hive.client.HiveClientImpl
-import com.qubole.shaded.hive.metastore.api.Table
-import com.qubole.shaded.hive.metastore.api.FieldSchema
-import org.apache.spark.SparkException
-import org.apache.spark.sql.catalyst.catalog.{BucketSpec, CatalogStorageFormat, CatalogTable, CatalogTableType, CatalogUtils}
-import org.apache.spark.sql.types._
+import org.apache.spark.sql.catalyst.analysis.UnresolvedAttribute
+import org.apache.spark.sql.catalyst.expressions.SpecificInternalRow
 import org.apache.spark.sql.catalyst.parser.{CatalystSqlParser, ParseException}
+import org.apache.spark.sql.execution.QueryExecution
+import org.apache.spark.sql.execution.datasources.HadoopFsRelation
+import org.apache.spark.sql.execution.datasources.text.TextFileFormat
+import org.apache.spark.sql.sources._
+import org.apache.spark.sql.types.{DataType, MetadataBuilder, StructField, StructType, _}
+import org.apache.spark.sql.util.QueryExecutionListener
 
 import scala.collection.JavaConversions._
-import scala.collection.mutable.ArrayBuffer
 
 class HiveAcidDataSource
   extends RelationProvider
     // with CreatableRelationProvider
-    // with PrunedFilteredScan
+    with PrunedFilteredScan
     with DataSourceRegister
     with Logging {
 
@@ -64,6 +62,10 @@ class HiveAcidDataSource
   var _dataSchema: StructType = _
   var _partitionSchema: StructType = _
 
+  var _sqlContext: SQLContext = _
+  var _broadcastedHadoopConf: Broadcast[SerializableConfiguration] = _
+  var _hTable: Table = _
+
 
   override def createRelation(
                                sqlContext: SQLContext,
@@ -72,18 +74,26 @@ class HiveAcidDataSource
       throw HiveAcidErrors.tableNotSpecifiedException
     })
 
-    _client = new HiveMetaStoreClient(new HiveConf(), null, false)
+    val hiveConf = HiveAcidDataSource.createHiveConf(sqlContext.sparkContext)
+    _client = new HiveMetaStoreClient(hiveConf, null, false)
+    val hive = Hive.get(hiveConf)
 
-    val hTable: Table = _client.getTable(tableName.split('.')(0), tableName.split('.')(1))
-    val cols = hTable.getSd.getCols
-    val partitionCols = hTable.getPartitionKeys
+    val table = _client.getTable(tableName.split('.')(0), tableName.split('.')(1))
+    _hTable = hive.getTable(tableName.split('.')(0), tableName.split('.')(1))
+
+    val cols = table.getSd.getCols
+    val partitionCols = table.getPartitionKeys
 
     _dataSchema = StructType(cols.toList.map(fromHiveColumn).toArray)
     _partitionSchema = StructType(partitionCols.map(fromHiveColumn).toArray)
+    _sqlContext = sqlContext
+    _broadcastedHadoopConf = _sqlContext.sparkContext.broadcast(
+      new SerializableConfiguration(_sqlContext.sparkContext.hadoopConfiguration))
+
 
 
     if (_acid_state == null) {
-      _acid_state = new HiveAcidFileIndex(sqlContext.sparkSession, hTable,
+      _acid_state = new HiveAcidFileIndex(sqlContext.sparkSession, table,
         sqlContext.sparkSession.sessionState.conf.defaultSizeInBytes, _client, _partitionSchema)
     }
 
@@ -151,14 +161,92 @@ class HiveAcidDataSource
     }
   }
 
-  //  override def buildScan(requiredColumns: Array[String], filters: Array[Filter]): RDD[Row] = {
-  //    // Rely on a type erasure hack to pass RDD[InternalRow] back as RDD[Row]
-  //    JDBCRDD.scanTable(
-  //      sparkSession.sparkContext,
-  //      schema,`
-  //      requiredColumns,
-  //      filters,
-  //      parts,
-  //      jdbcOptions).asInstanceOf[RDD[Row]]
-  //  }
+  override def buildScan(requiredColumns: Array[String], filters: Array[Filter]): RDD[Row] = {
+
+    val tableDesc = new TableDesc(_hTable.getInputFormatClass, _hTable.getOutputFormatClass,
+      _hTable.getMetadata)
+    val initializeJobConfFunc = HiveAcidDataSource.initializeLocalJobConfFunc(
+      _hTable.getPath.toString , tableDesc) _
+    val ifc = _hTable.getInputFormatClass
+      .asInstanceOf[java.lang.Class[InputFormat[Writable, Writable]]]
+    val _minSplitsPerRDD = math.max(
+      _sqlContext.sparkContext.hadoopConfiguration.getInt("mapreduce.job.maps", 1),
+      _sqlContext.sparkContext.defaultMinPartitions)
+    val deserializerClassName = tableDesc.getSerdeClassName.replaceFirst(
+      "org.apache.hadoop.hive.", "com.qubole.shaded.hive.")
+    val colNames = getColumnNamesFromFieldSchema(_hTable.getSd.getCols)
+    val colTypes = getColumnTypesFromFieldSchema(_hTable.getSd.getCols)
+
+    val allHiveCols = _hTable.getAllCols
+    val requiredHiveFields = requiredColumns.map(x => allHiveCols.find(_.getName == x).get)
+
+    val dataTypes = requiredHiveFields.map {
+      case x => HiveAcidDataSource.getCatalystDatatypeFromHiveDatatype(x.getType)
+    }
+    val mutableRow = new SpecificInternalRow(dataTypes)
+    val attrsWithIndex = requiredHiveFields.map { x => UnresolvedAttribute(x.getName)}.zipWithIndex
+
+    new HiveRDD(_sqlContext.sparkContext,
+      _broadcastedHadoopConf,
+      Some(initializeJobConfFunc),
+      ifc,
+      new java.lang.Integer(_minSplitsPerRDD),
+      deserializerClassName, mutableRow, attrsWithIndex, tableDesc.getProperties,
+      colNames, colTypes
+    )
+  }.asInstanceOf[RDD[Row]]
+}
+
+object HiveAcidDataSource extends Logging {
+  def initializeLocalJobConfFunc(path: String, tableDesc: TableDesc)(jobConf: JobConf) {
+    FileInputFormat.setInputPaths(jobConf, Seq[Path](new Path(path)): _*)
+    if (tableDesc != null) {
+      HiveAcidDataSource.configureJobPropertiesForStorageHandler(tableDesc, jobConf, true)
+      Utilities.copyTableJobPropertiesToConf(tableDesc, jobConf)
+    }
+    val bufferSize = System.getProperty("spark.buffer.size", "65536")
+    jobConf.set("io.file.buffer.size", bufferSize)
+  }
+
+  def configureJobPropertiesForStorageHandler(
+                                               tableDesc: TableDesc, conf: Configuration, input: Boolean) {
+    val property = tableDesc.getProperties.getProperty(
+      org.apache.hadoop.hive.metastore.api.hive_metastoreConstants.META_TABLE_STORAGE
+    )
+    val storageHandler =
+      HiveUtils.getStorageHandler(conf, property)
+    if (storageHandler != null) {
+      val jobProperties = new java.util.LinkedHashMap[String, String]
+      if (input) {
+        storageHandler.configureInputJobProperties(tableDesc, jobProperties)
+      } else {
+        storageHandler.configureOutputJobProperties(tableDesc, jobProperties)
+      }
+      if (!jobProperties.isEmpty) {
+        tableDesc.setJobProperties(jobProperties)
+      }
+    }
+  }
+
+  def getCatalystDatatypeFromHiveDatatype(hiveDatatype: String): DataType = {
+    hiveDatatype match {
+      case "" => IntegerType
+      case _ => StringType
+    }
+  }
+
+  def createHiveConf(sparkContext: SparkContext): HiveConf = {
+    val hiveConf = new HiveConf()
+    (sparkContext.hadoopConfiguration.iterator().map(kv => kv.getKey -> kv.getValue)
+      ++ sparkContext.getConf.getAll.toMap).foreach { case (k, v) =>
+      logDebug(
+        s"""
+           |Applying Hadoop/Hive/Spark and extra properties to Hive Conf:
+           |$k=${if (k.toLowerCase(Locale.ROOT).contains("password")) "xxx" else v}
+         """.stripMargin)
+      hiveConf.set(k, v)
+    }
+    hiveConf
+  }
+
 }
